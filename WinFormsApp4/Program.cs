1d0a7d436419dfa2947525c210b0e1c51fc9bc88
Akuma <102458052+KatrinMilson@users.noreply.github.com>
2025-02-01 17:10:00 +0300

    Add files via upload

---
+++ WinFormsApp4/Program.cs
@@ -0,0 +1,34 @@
+namespace WinFormsApp4
+{
+    using System;
+    using System.Windows.Forms;
+    using System.Drawing;
+    class FormWithWorkingButton : Form
+    {
+        Button mrButton;
+        // Метод-конструктор нашего класса 
+        public FormWithWorkingButton()
+        {
+            this.Text = "Форма с работающей кнопкой!"; // Указываем заголовок окна 
+                                                       // Добавляем кнопку и привязываем ее к обработчику события 
+            mrButton = new Button();
+            mrButton.Text = "Нажми меня";
+            mrButton.Top = 100;
+            mrButton.Left = 100;
+            mrButton.Height = 50;
+            mrButton.Width = 70;
+            mrButton.Click += new System.EventHandler(mrButton_Click);
+            this.Controls.Add(mrButton);
+        }
+        static void Main()
+        {
+            // Создаем и запускаем форму 
+            Application.Run(new FormWithWorkingButton());
+        }
+        // Обработчик события, срабатывающий при нажатии кнопки 
+        void mrButton_Click(object sender, EventArgs e)
+        {
+            mrButton.Text = "Кнопка была нажата!"; // Изменяем текст 
+        }
+    }
+}
