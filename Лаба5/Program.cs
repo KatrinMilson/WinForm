ad0469105837f0ee7318a1c38e6a9e2ded1df408
Akuma <102458052+KatrinMilson@users.noreply.github.com>
2025-02-01 17:14:06 +0300

    Add files via upload

---
+++ Лаба5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class ValueInput
+{
+    public static T ReadValue<T>(string prompt)
+    {
+        Console.Write(prompt);
+        string input = Console.ReadLine();
+
+        try
+        {
+            return (T)Convert.ChangeType(input, typeof(T));
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Ошибка ввода. Пожалуйста, введите корректное значение.");
+            return ReadValue<T>(prompt);
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Программа для ввода значений:");
+
+        string word = ValueInput.ReadValue<string>("Введите слово: ");
+        int number = ValueInput.ReadValue<int>("Введите целое число: ");
+        double decimalNumber = ValueInput.ReadValue<double>("Введите дробное число: ");
+
+        Console.WriteLine($"Вы ввели: {word}, {number}, {decimalNumber}");
+    }
+}
