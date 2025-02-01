ab6a574a53c04a5a1f01c24662785d13d3b792d8
Akuma <102458052+KatrinMilson@users.noreply.github.com>
2025-02-01 19:14:31 +0300

    Add files via upload

---
+++ PM3/Logger.cs
@@ -0,0 +1,19 @@
+﻿using log4net;
+using log4net.Config;
+
+namespace PM3
+{
+    public static class Logger
+    {
+        private static readonly ILog log = LogManager.GetLogger("LOGGER");
+        private static readonly ILog errorLog = LogManager.GetLogger("ERROR_LOGGER");
+
+        public static ILog Log => log;
+        public static ILog ErrorLog => errorLog;
+
+        public static void InitLogger()
+        {
+            XmlConfigurator.Configure();
+        }
+    }
+}
---
+++ PM3/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Web;
+
+
+namespace PM3
+{
+    internal class Program
+    {
+        static string Out_name(String a)
+        {
+            return Path.GetFileName(a);
+        }
+
+        static void Main(string[] args)
+        {
+
+            Logger.InitLogger();
+            string filePath = "C:\\Users\\satoa\\source\\repos\\PM3\\bin\\Debug\\net6.0\\PM.txt"; // Путь к файлу, который ты хочешь проверить
+            CheckFileAccess(filePath);
+            //string out_name = Path.GetFileName(filePath);
+        }
+
+        static void CheckFileAccess(string filePath)
+        {
+            try
+            {
+                if (!File.Exists(filePath))
+                {
+                    Logger.ErrorLog.Error($"Файл {filePath} не найден по указанному пути");
+                    return;
+                }
+
+                Logger.Log.Info($"Файл {filePath} пути найден, производится раскрытие в самом существе");
+
+                using (FileStream fileStream = File.OpenRead(filePath))
+                {
+                    Logger.Log.Info($" {Out_name(filePath)} раскрытие в существе произведено успешно");
+
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        string content = reader.ReadToEnd();
+
+                        Logger.Log.Info($"Содержимое файла {Out_name(filePath)}: {content}");
+                    }
+                }
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                Logger.ErrorLog.Error($"Нет доступа к файлу {Out_name(filePath)}: {ex.Message}");
+            }
+            catch (IOException ex)
+            {
+                Logger.ErrorLog.Error($"Ошибка Доступ к файлу запрещён {Out_name(filePath)}: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Logger.ErrorLog.Error($"Произошла ошибка при раскрытии в самом существе {Out_name(filePath)}: {ex.Message}");
+            }
+        }
+    }
+
+}
